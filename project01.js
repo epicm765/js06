cd5ec74a293fc314eaa512e76ea6faf790156966
epic <ggj03102@gmail.com>
2021-01-26 19:57:36 +0900

    project01

---
+++ project01.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const startChat = document.getElementById('btn').addEventListener('click', ()=>{
+    window.open('http://google.com/');
+});
+
+const popUp1 = document.getElementById('btn2').addEventListener('click', () => {
+    alert('준비 중 입니다.');
+});
+
+const popUp2 = document.getElementById('btn3').addEventListener('click', () => {
+    alert('준비 중 입니다.');
+});
